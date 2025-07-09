e72c200d7219579be108873e6559457d06ed5cf3
Jean Neves <jeanevess@gmail.com>
2025-07-09 20:51:46 -0300

    correcao de conexao pela rede da casa

--- frontend/src/Services/Auth.ts
+++ frontend/src/Services/Auth.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export async function login(usuario: string, senha: string) {
-  const response = await axios.post('http://localhost:3000/usuarios/login', { usuario, senha });
+  const response = await axios.post('http://192.168.1.42:3000/usuarios/login', { usuario, senha });
   return response.data;
 }
  
