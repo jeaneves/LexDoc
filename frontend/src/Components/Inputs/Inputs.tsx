d4624a792b19977b839ebe95ca547bd18f295fac
Jean Neves <jeanevess@gmail.com>
2025-07-12 15:47:50 -0300

    criação do form de cadastro do forum

--- frontend/src/Components/Inputs/Inputs.tsx
+++ frontend/src/Components/Inputs/Inputs.tsx
@@ -1,3 +1,5 @@
+
+
 type Props = {
   id?: string;
   name?: string;
@@ -5,15 +7,24 @@ type Props = {
   placeholder?: string;
   value?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  uppercase?: string;
+  className?: string;
 };
 
-export function Input({ id, name, type, placeholder, value, onChange,uppercase }: Props) {
+
+export function Input({
+  id,
+  name,
+  type,
+  placeholder,
+  value,
+  onChange,
+  className
+}: Props) {
   return (
     <input
-      id={id} // ✅ adicionado aqui
-      className={`${uppercase ?? ''} shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline`}
-      name={name} // ✅ adicionado aqui      
+      id={id}
+      className={`${className ?? ''} shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline`}
+      name={name}
       type={type}
       placeholder={placeholder}
       value={value}
