76c3376059b217494cb912fb84b1ed681ea87258
Jean Neves <jeanevess@gmail.com>
2025-07-13 12:53:42 -0300

    ok

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,8 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,  // equivale a '0.0.0.0'
+    host: '0.0.0.0',  // equivale a '0.0.0.0'
     port: 5173,  // pode mudar se quiser outra porta
+    strictPort: true // Dá erro se a porta estiver ocupada, ao invés de trocar
   }
   
 })
